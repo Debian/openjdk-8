5f0b3ae857027ba3e460d592f1576c8aab3b486b
Matthias Klose <doko@ubuntu.com>
2008-07-23 15:35:26 +0200

    * debian/buildwatch.sh: Track long building files with no visible output.

--- buildwatch.sh
+++ buildwatch.sh
@@ -1,15 +1,31 @@
 #! /bin/sh
 
-# powerpc build takes up to 10h (600min)
-wait=$(expr 600 \* 60)
-ival=$(expr 15 \* 60)
-#wait=5
-#ival=1
+maxwait=$(expr 180 \* 60)
+wait=$maxwait
+ival=$(expr 30 \* 60)
+#ival=3
 
 while [ $wait -gt 0 ]; do
     sleep $ival
-    if ps x | grep -v grep | egrep -qs '/cc1|jar|java'; then
-	echo "compiler/java/jar running ..."
-    fi
     wait=$(expr $wait - $ival)
+    state=
+    if ps x | grep -v grep | egrep -qs '/cc1|jar|java|gij'; then
+	state="compiler/java/jar running ..."
+	wait=$maxwait
+    fi
+
+    new_quiet=$(ls -l openjdk*/control/build/*/tmp/rt-orig.jar 2>&1 | md5sum)
+    if [ "$old_quiet" != "$new_quiet" ]; then
+	state="assembling rt.jar ..."
+	wait=$maxwait
+    fi
+    old_quiet=$new_quiet
+
+    new_noisy=$(ls -l mauve_output jtreg_output 2>&1 | md5sum)
+    if [ "$old_noisy" != "$new_noisy" ]; then
+	wait=$maxwait
+    elif [ -n "$state" ]; then
+	echo $state
+    fi
+    old_noisy=$new_noisy
 done
