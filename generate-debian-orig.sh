f7d2e300600416c307ee77723d2dc9d7dfe0b981
Damien Raude-Morvan <drazzib@debian.org>
2011-07-15 23:04:30 +0200

    * New b147 code drop (OpenJDK7 RC1). * d/patches/zero-jsr292-fixes.diff: Fixes on Zero/Shark for JSR 292 support   from Chris Phillips <ChrisPhi@lgonqn.org>.

--- generate-debian-orig.sh
+++ generate-debian-orig.sh
@@ -3,8 +3,8 @@
 #tarballs="corba.tar.gz hotspot.tar.gz jaxp.tar.gz jaxws.tar.gz jdk-dfsg.tar.gz langtools.tar.gz openjdk.tar.gz"
 tarballs="corba.tar.gz hotspot.tar.gz jaxp.tar.gz jaxws.tar.gz jdk.tar.gz langtools.tar.gz openjdk.tar.gz"
 jamvmtb=jamvm-f8b8e8e78ec057a5852ff8c0f3386b48f3eca907.tar.gz
-tarballdir=b143
-version=7~b143-2.0~pre1
+tarballdir=b147
+version=7~b147-2.0~pre1
 base=openjdk-7
 pkgdir=$base-$version
 origtar=${base}_${version}.orig.tar.gz
