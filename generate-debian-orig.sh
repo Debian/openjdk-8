783ba4604d5ef50a1104d3255f9d1b1ed08c3dc2
Matthias Klose <doko@18xx.org>
2013-04-22 12:03:29 +0200

    openjdk-7 (7u21-2.3.9-1) unstable; urgency=high

--- generate-debian-orig.sh
+++ generate-debian-orig.sh
@@ -3,8 +3,8 @@
 tarballs="corba.tar.gz hotspot-default.tar.gz hotspot-zero.tar.gz jaxp.tar.gz jaxws.tar.gz jdk-dfsg.tar.gz langtools-dfsg.tar.gz openjdk.tar.gz"
 jamvmtb=jamvm-0972452d441544f7dd29c55d64f1ce3a5db90d82.tar.gz
 cacaotb=
-tarballdir=7u17
-version=7u17-2.3.8
+tarballdir=7u21
+version=7u21-2.3.9
 base=openjdk-7
 pkgdir=$base-$version
 origtar=${base}_${version}.orig.tar.gz
